94fc66f1c3360dca77687995c9737c81b2a685d6
cleochig <cleo.chig@gmail.com>
2015-09-14 17:20:40 -0300

    Atualização

--- clima/www/js/app.js
+++ clima/www/js/app.js
@@ -1,56 +1,68 @@
-var lerClimas = angular.module("lerClimas", ["ionic"]);
+var climaApp = angular.module("climaApp",["ionic"]);
 
-lerClimas.service("lerClimasSvc", ["$http", "$rootScope", lerClimasSvc]);
+// Criando o serviço e injetando dependencias
+climaApp.service("obterClimaSvc",["$http","$rootScope",obterClimaSvc]);
+// Criando o Controlador e injetando dependencias
+climaApp.controller("climaCtrl",["$scope","obterClimaSvc",climaCtrl]);
 
-lerClimas.controller("lerClimasCtrl", ["$scope","$sce","$ionicLoading","$ionicListDelegate","$ionicPlatform","lerClimasSvc", lerClimasCtrl]);
+function obterClimaSvc($http, $rootScope){
 
-function lerClimasCtrl($scope, $sce, $ionicLoading, $ionicListDelegate, $ionicPlatform, lerClimasSvc) {
-
-    $ionicLoading.show({template: "Carregando..."});
-
-    $scope.deviceReady = false;
-
-    $ionicPlatform.ready(function() {
-        $scope.$apply(function() {
-            $scope.deviceReady = true;
+    this.loadClima = function(params){
+        $http.get("http://api.openweathermap.org/data/2.5/weather", {params: params}).success(
+                function(result){
+                    $rootScope.$broadcast("climaApp.clima",result);
+            }
+        ).error(function(result) {
+             alert("Requisição Falhou");
         });
+    }
+}
+
+function climaCtrl ($scope, obterClimaSvc){
+
+    $scope.params = {q:"Lins"};
+
+    $scope.$on("climaApp.clima", function(_, result) {
+
+        console.log(result.name);
+        console.log(result.sys.country);
+        console.log(result.coord.lat);
+        console.log(result.coord.lon);
+        console.log(result.main.temp);
+        console.log(result.main.humidity);
+        console.log(result.main.pressure);
+        console.log(result.wind.speed);
+        console.log(result.weather.description);
+        console.log(result.weather.icon);
+
+        $scope.name = result.name;
+        $scope.country = result.sys.country;
+
+        $scope.lat = result.coord.lat;
+        $scope.lon = result.coord.lon;
+
+        $scope.temp = result.main.temp;
+        $scope.humidity = result.main.humidity;
+        $scope.pressure = result.main.pressure;
+
+        $scope.speed = result.wind.speed;
+
+        $scope.humidity = result.weather.description;
+        $scope.icon = result.weather.icon;
+
+
+
     });
 
-    $scope.climas = [];
-    $scope.params = {};
+    obterClimaSvc.loadClima($scope.params);
 
-    $scope.$on("lerClimas.climas", function(_, result) {
-        result.forEach(function(b) {
-            console.log(b.name);
-            $scope.climas.push({
-                name: b.name,
-
-            });
-        });
-
-	$scope.$broadcast("scroll.infiniteScrollComplete");
-			$scope.$broadcast("scroll.refreshComplete");
-
-	});
-
-    $scope.loadMore = function() {
-        lerClimasSvc.loadClimas($scope.params);
-        $ionicLoading.hide();
-    }
-    $scope.reload = function() {
-        $scope.climas = [];
-        $scope.params = {};
-        lerClimasSvc.loadClimas();
+    $scope.obterClimaParam = function(p_city){
+        console.log(p_city);
+        if (p_city!=undefined){
+            obterClimaSvc.loadClima({q:p_city});
+        }
     }
 
 
 }
 
-function lerClimasSvc($http, $rootScope) {
-    this.loadClimas = function(params) {
-        $http.get("http://api.openweathermap.org/data/2.5/weather?q=lins", {params: params})
-            .success(function(result) {
-                $rootScope.$broadcast("lerClimas.climas", result);
-            });
-    }
-}
